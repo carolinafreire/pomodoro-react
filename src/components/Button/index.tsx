df87b280844c8c4c1366328bb83736ca01bb22da
Carol Freire <68876784+carolinafreire@users.noreply.github.com>
2022-08-08 13:15:03 -0300

    Delete src directory

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import style from "./Button.module.scss";
-
-function Button({ buttonTitle }: { buttonTitle: string }) {
-  return <button className={style.button}>{buttonTitle}</button>;
-}
-
-export default Button;
